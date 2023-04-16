4bb5bf3648d9bd04080f7702f4b5eee3dfb197ff
onlystudies888 <129259267+onlystudies888@users.noreply.github.com>
2023-04-16 16:44:27 +0530

    added codes

---
+++ index.js
@@ -0,0 +1,50 @@
+var buttonLength = document.querySelectorAll(".drum").length;
+
+document.addEventListener("keydown", function (event) {
+   music(event.key);
+
+})
+
+for (let i = 0; i < buttonLength; i++) {
+   var buttonS = document.querySelectorAll(".drum")[i];
+   buttonS.addEventListener("click", function () {
+      let eventS = this.innerHTML;
+      // console.log(this);
+      music(eventS);
+
+
+
+   })
+}
+function music(eventS) {
+
+   switch (eventS) {
+      case 'w':
+         var song = new Audio("sounds/tom-1.mp3");
+         song.play();
+         break;
+      case 'a':
+         var song = new Audio("sounds/tom-2.mp3");
+         song.play();
+         break;
+      case 's':
+         var song = new Audio("sounds/tom-3.mp3");
+         song.play();
+         break; case 'd':
+         var song = new Audio("sounds/tom-4.mp3");
+         song.play();
+         break; case 'j':
+         var song = new Audio("sounds/kick-bass.mp3");
+         song.play();
+         break; case 'k':
+         var song = new Audio("sounds/crash.mp3");
+         song.play();
+         break; case 'l':
+         var song = new Audio("sounds/snare.mp3");
+         song.play();
+         break;
+
+      default:
+         break;
+   }
+}
